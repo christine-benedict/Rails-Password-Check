c0f2731607521ef5cc38aefb77cfbad39a4256b1
learn <learn@learn08.localdomain>
2018-04-24 14:46:39 -0700

    fixed regex for checking to ensure password has a number

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,11 +4,10 @@ class PasswordsController < ApplicationController
         @userid = params[:userid]
         @password = params[:password]
 
-        if @userid == @password
-            # && /0-9/.match(params[:password]) == true
-            flash.now[:notice] ="User ID and password cannot match and password must contain a number."
-        else
+        if @userid != @password && @password =~ /\d/
             flash.now[:notice] = "Credentials are acceptable."
+        else
+            flash.now[:notice] ="User ID and password cannot match and password must contain a number."
         end
         render 'check.html.erb'
     end
